27c1fc842a226fd780ec982a81dc9ca41fbf7853
van36033-sudo <van36033@gmail.com>
2026-01-03 10:33:05 +0700

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/bk-deep-learning-web/',
-  plugins: [react()]
+  base: "/bk-deep-learning/",
+  plugins: [react()],
 });
